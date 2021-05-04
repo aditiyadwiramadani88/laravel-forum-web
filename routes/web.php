a3b0ff22ac3484230464f437e60aba3aa55c6f8e
adit <aditiyadwiramadani88@gmail.com>
2021-05-04 11:27:18 +0700

    laravel forum web

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Question;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckAuth;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,22 +32,27 @@ Route::get("logout", function(Request $request) {
 });                              
 
 
-// question
-
 Route::any("ask_question",[Question::class, "AskQuestion"]);
 Route::get('search', [Question::class, 'SearchQuestion']);
 Route::any('/detail_question/{id}', [Question::class, 'DetailQuestion'])->where('id', '[1-9]+');
 
 
+
+Route::middleware(CheckAuth::class)->prefix('/')->group(function() { 
+
+// question
 Route::any('/edit_question/{id}', [Question::class, 'EditQuestion'])->where('id', '[1-9]+');
 Route::any('delete_question/{id}', [Question::class, 'DeleteQuestion'])->where('id', '[1-9]+');
 Route::get('your_question', [Question::class, 'YourQuestion']);
-
 
 // answere 
 Route::post('new_answere', [Answere::class, 'CreateAnswere']);
 Route::get('/delete_answere/{id}/{id_question}', [Answere::class, 'DeleteAnswere'])->where('id', '[1-9]+')->where('id_question', '[1-9]+');
 Route::post('/edit_answere/{id}', [Answere::class, 'EditAnswere'])->where('id', '[1-9]+');
+});
+
+
+
 
 
 Route::get('/home', function() { 
